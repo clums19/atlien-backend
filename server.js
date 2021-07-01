8735c3daadb82d328af06610c274c9299d9394dd
Chelsea L <chelsealumsden@gmail.com>
2021-07-01 17:15:46 -0400

    WIP: mongoose is connected

--- server.js
+++ server.js
@@ -2,9 +2,23 @@
 // ---------------------------------
 require('dotenv').config();
 
-const { PORT = 4000} = process.env;
+const { PORT = 4000, MONGODB_URL} = process.env;
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Database Connection
+// ----------------------------------
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+
+mongoose.connection
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
+
 
 // Middleware 
 // ----------------------------------
