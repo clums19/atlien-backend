3f69adccb30528ba88cca884c7b2a107291fc811
Chelsea L <chelsealumsden@gmail.com>
2021-07-01 21:21:55 -0400

    WIP: bar and food schema created

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { PORT = 4000, MONGODB_URL} = process.env;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
 
 // Database Connection
 // ----------------------------------
@@ -13,6 +15,41 @@ mongoose.connect(MONGODB_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 });
+
+// Models
+// ----------------------------------
+const FoodSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    description: String,
+    distance: String,
+    web: String,
+    category: String,
+
+})
+const Food = mongoose.model('Food', FoodSchema);
+
+const BarSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    description: String,
+    distance: String,
+    web: String,
+    category: String,
+
+})
+const Bar = mongoose.model('Bar', BarSchema);
+
+const ClubSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    description: String,
+    distance: String,
+    web: String,
+    category: String,
+
+})
+const Club = mongoose.model('Club', ClubSchema);
 
 mongoose.connection
     .on("open", () => console.log("You are connected to mongoose"))
@@ -22,12 +59,63 @@ mongoose.connection
 
 // Middleware 
 // ----------------------------------
-
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
 
 // Routes
 // ----------------------------------
+// Test
 app.get('/', (req, res) => {
     res.send('ATLien')
+});
+
+// Index
+// ------------------------------------
+app.get('/food', async(req, res) => {
+    try {
+        res.json(await Food.find({}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+app.get('/bars', async(req, res) => {
+    try {
+        res.json(await Bar.find({}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+app.get('/clubs', async(req, res) => {
+    try {
+        res.json(await Club.find({}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+// Create
+// ------------------------------------------
+app.post('/food', async(req, res) => {
+    try {
+        res.json(await Food.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+app.post('/bars', async(req, res) => {
+    try {
+        res.json(await Bar.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+app.post('/clubs', async(req, res) => {
+    try {
+        res.json(await Club.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
 });
 
 // Listener
